f65f2acf9111e2b8fe6d1303f9b08e75d28ff8d7
beorn <beorn_d_latch@yahoo.com>
2018-07-28 00:37:02 +0200

    Busy with routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,15 @@ import { UserComponent } from './users/user/user.component';
 import { EditServerComponent } from './servers/edit-server/edit-server.component';
 import { ServerComponent } from './servers/server/server.component';
 import { ServersService } from './servers/servers.service';
+import {RouterModule, Routes} from "@angular/router";
+
+
+const appRoutes : Routes = [
+  {path : '', component : HomeComponent},
+  {path : 'servers', component : ServersComponent},
+  {path : 'home', component : HomeComponent},
+  {path : 'users', component : UserComponent}
+];
 
 @NgModule({
   declarations: [
@@ -25,7 +34,8 @@ import { ServersService } from './servers/servers.service';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [ServersService],
   bootstrap: [AppComponent]
