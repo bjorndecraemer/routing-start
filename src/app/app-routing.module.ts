cc0b1cfe91fa6be2e1ef9fa085f4c0fe22de9a88
beorn <beorn_d_latch@yahoo.com>
2018-08-09 18:01:14 +0200

    Finished routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,11 +6,11 @@ import {ServerComponent} from "./servers/server/server.component";
 import {EditServerComponent} from "./servers/edit-server/edit-server.component";
 import {UsersComponent} from "./users/users.component";
 import {UserComponent} from "./users/user/user.component";
-import {PageNotFoundComponent} from "./page-not-found/page-not-found.component";
 import {NgModule} from "@angular/core";
 import {AuthGuard} from "./route-guard.service";
 import {CanDeactivateGuard} from "./servers/edit-server/can-deactivate-guard.service";
 import {ErrorPageComponent} from "./error-page/error-page.component";
+import {ServerResolver} from "./servers/server/server-resolver.service";
 
 const appRoutes : Routes = [
   {path : '', component : HomeComponent},
@@ -18,7 +18,7 @@ const appRoutes : Routes = [
     canActivateChild: [AuthGuard],
     component : ServersComponent,
     children: [
-      {path : ':id', component : ServerComponent},
+      {path : ':id', component : ServerComponent, resolve: {server: ServerResolver}},
       {path : ':id/edit', component : EditServerComponent, canDeactivate : [CanDeactivateGuard] }
     ]},
   {path : 'home', component : HomeComponent},
